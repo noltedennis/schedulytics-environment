112e5005749190de4f33078abcc27010df903c2d
noltedennis <dennis_nolte@gmx.de>
2020-06-06 20:54:30 +0200

    Bugfixes

--- build/kustomize.py
+++ build/kustomize.py
@@ -38,7 +38,7 @@ kustomize_dir = Path('kustomize', build_target)
 
 # Read stdin and write to file
 helm_out = str(kustomize_dir / 'all.yaml')
-with open('kustomize/prod/all.yaml', 'w') as text_file:
+with open(helm_out, 'w') as text_file:
     text_file.write(sys.stdin.read())
 
 # Execute kustomize on that and store result
