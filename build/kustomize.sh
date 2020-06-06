bfcf2571dec36bd8710df941d063a84e661b0b4f
noltedennis <dennis_nolte@gmx.de>
2020-06-06 10:26:22 +0200

    Move from symlinks to dir directives

--- build/kustomize.sh
+++ build/kustomize.sh
@@ -10,9 +10,8 @@
 #   BUILDTARGET         either "local" or "PROD"
 ############################################################################
 
-# Runtime information
-DIR=$(dirname $0)
-
+# Runtime information, base everything on caller directory
+DIR=$(pwd)
 
 if [ -z ${BUILDTARGET} ]; then
   echo "Variable BUILDTARGET must be defined"
@@ -24,5 +23,3 @@ KUSTOMIZE_DIR=${DIR}/${BUILDTARGET}
 cat <&0 > ${KUSTOMIZE_DIR}/all.yaml
 
 kubectl kustomize ${KUSTOMIZE_DIR}
-
-rm ${KUSTOMIZE_DIR}/all.yaml
