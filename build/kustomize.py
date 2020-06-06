901e11c42b02485c8fb9d6c1595e2a6d1e999aeb
noltedennis <dennis_nolte@gmx.de>
2020-06-06 16:31:45 +0200

    Switch post-processor for Helm to Python

---
+++ build/kustomize.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+"""
+kustomize.py
+This script is designed to be used as environment-independant
+post-renderer for Helm. It will take stdin and include it in its
+kustomize build of the relative directory kustomize/${BUILDTARGET}.
+In addition, it will order certain resource kinds to be created first,
+because kustomize currently lacks inter-dependency ordering.
+
+Required inputs:
+  STDIN       - Output from Helm template
+  BUILDTARGET - Environment variable, one of "local" or "prod"
+
+Output:
+  STDOUT      - A resource definition run through kustomize & custom
+                ordering
+
+Requires:
+  - Local installation of kustomize (in PATH)
+  - Python 3.4 or higher
+"""
+
+import os
+import subprocess
+import sys
+import yaml
+
+from pathlib import Path
+
+# Read environment variable for environment
+build_target = os.getenv('BUILDTARGET')
+if build_target != 'local' and build_target != 'prod':
+    print("Environment variable BUILDTARGET must be set to either 'local' or 'prod'")
+    sys.exit(1)
+kustomize_dir = Path('kustomize', build_target)
+
+# Read stdin and write to file
+helm_out = kustomize_dir / 'all.yaml'
+with open(helm_out, 'w') as text_file:
+    text_file.write(sys.stdin.read())
+
+# Execute kustomize on that and store result
+kustomize_out = subprocess.check_output(["kubectl", 'kustomize', str(kustomize_dir)], shell=True)
+
+# Read results and output them according to priorization
+ordered_kinds = ["CustomResourceDefinition" "ValidatingWebhookConfiguration"]
+
+for x in yaml.load_all(kustomize_out):
+    if x['kind'] in ordered_kinds:
+      print("---")
+      print(yaml.dump(x))
+
+for x in yaml.load_all(kustomize_out):
+    if x['kind'] not in ordered_kinds:
+      print("---")
+      print(yaml.dump(x))
+
+# Cleanup
+os.remove(helm_out)
